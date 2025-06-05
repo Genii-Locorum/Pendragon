49c6d9cd0bcf6f9446bac2a825b44a8c88257237
John C Barstow <jcb@jbowtie.geek.nz>
2025-06-05 16:33:22 +1200

    Migrate culture, gear, and squire to AppV2

--- module/item/sheets/squire.mjs
+++ module/item/sheets/squire.mjs
@@ -1,66 +1,91 @@
-import { addPIDSheetHeaderButton } from '../../pid/pid-button.mjs'
 import {PENSelectLists}  from "../../apps/select-lists.mjs";
+import { PendragonItemSheet } from "./item-sheet.mjs";
 
-export class PendragonSquireSheet extends ItemSheet {
-  constructor (...args) {
-    super(...args)
-    this._sheetTab = 'items'
-  }
-  
-  //Add PID buttons to sheet
-  _getHeaderButtons () {
-    const headerButtons = super._getHeaderButtons()
-    addPIDSheetHeaderButton(headerButtons, this)
-    return headerButtons
+export class PendragonSquireSheet extends PendragonItemSheet {
+  constructor (options = {}) {
+    super(options)
   }
 
-  static get defaultOptions () {
-    return foundry.utils.mergeObject(super.defaultOptions, {
-      classes: ['Pendragon', 'sheet', 'item'],
+  static DEFAULT_OPTIONS = {
+    classes: ['Pendragon', 'sheet', 'item'],
+    position: {
       width: 550,
-      height: 470,
-      scrollY: ['.item-bottom-panel'],
-      tabs: [{navSelector: '.sheet-tabs',contentSelector: '.sheet-body',initial: 'attributes'}]
-    })
+      height: 470
+    },
+    tag: "form",
+    // automatically updates the item
+    form: {
+      submitOnChange: true,
+    },
+    window: {
+      resizable: true,
+    },
+    actions: {
+      onEditImage: this._onEditImage,
+      editPid: this._onEditPid
+    }
+
   }
-  
-  /** @override */
-  get template () {
-    return `systems/Pendragon/templates/item/${this.item.type}.html`
+
+  static PARTS = {
+    header: {
+      template: "systems/Pendragon/templates/item/header.hbs"
+    },
+    tabs: {
+      template: 'templates/generic/tab-navigation.hbs',
+    },
+    // each tab gets its own template
+    attributes: {
+      template: 'systems/Pendragon/templates/item/squire.attributes.hbs'
+    },
+    description: {
+      template: 'systems/Pendragon/templates/item/base.description.hbs'
+    },
+    gmTab: {
+      template: 'systems/Pendragon/templates/item/gmtab.hbs'
+    }
   }
-  
-  async getData () {
-    const sheetData = super.getData()
-    const itemData = sheetData.item
-    sheetData.hasOwner = this.item.isEmbedded === true
-    sheetData.isGM = game.user.isGM
-    sheetData.squireType = await PENSelectLists.getSquireTypes();
+
+  async _prepareContext (options) {
+    // Default tab for first time it's rendered this session
+    if (!this.tabGroups.primary) this.tabGroups.primary = 'attributes';
+
+    let sheetData = {
+      ...await super._prepareContext(options),
+    }
+    sheetData.squireType = PENSelectLists.getSquireTypes();
+
+    // these two values could be set during _preparePartContext
     sheetData.enrichedDescriptionValue = await TextEditor.enrichHTML(
-      sheetData.data.system.description,
+      this.item.system.description,
+      {
+        async: true,
+        secrets: sheetData.editable,
+        relativeTo: this.item
+      }
+    )
+    sheetData.enrichedGMDescriptionValue = await TextEditor.enrichHTML(
+      this.item.system.GMdescription,
       {
         async: true,
         secrets: sheetData.editable
       }
     )
-    sheetData.enrichedGMDescriptionValue = await TextEditor.enrichHTML(
-      sheetData.data.system.GMdescription,
-      {
-        async: true,
-        secrets: sheetData.editable
-      }
-    )    
+    sheetData.tabs = this._initTabs('primary', ['attributes', 'description', 'gmTab']);
     return sheetData
   }
-  
-  /* -------------------------------------------- */
-  
-  /**
-  * Activate event listeners using the prepared sheet HTML
-  * @param html {HTML}   The prepared HTML object ready to be rendered into the DOM
-  */
-  activateListeners (html) {
-    super.activateListeners(html)
-    // Everything below here is only needed if the sheet is editable
-    if (!this.options.editable) return
+
+  // this does the minimum currently, just sets the tab
+  // could also prepare tab-specific fields
+  async _preparePartContext(partId, context) {
+    switch (partId) {
+      case 'attributes':
+      case 'description':
+      case 'gmTab':
+        context.tab = context.tabs[partId];
+        break;
+      default:
+    }
+    return context;
   }
 }
