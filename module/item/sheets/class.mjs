7c94944711150cefadcc090ffed801a3be4513e8
John C Barstow <jcb@jbowtie.geek.nz>
2025-06-08 16:39:58 +1200

    Convert remaining items to Appv2

--- module/item/sheets/class.mjs
+++ module/item/sheets/class.mjs
@@ -1,53 +1,64 @@
 import { addPIDSheetHeaderButton } from '../../pid/pid-button.mjs'
 import { PENUtilities } from "../../apps/utilities.mjs"
+import { PendragonItemSheet } from "./item-sheet.mjs";
 
-export class PendragonClassSheet extends ItemSheet {
-  constructor (...args) {
-    super(...args)
-    this._sheetTab = 'items'
+export class PendragonClassSheet extends PendragonItemSheet {
+  #dragDrop;
+  constructor (options = {}) {
+    super(options);
+    this.#dragDrop = this.#createDragDropHandlers();
   }
 
-  //Add PID buttons to sheet
-  _getHeaderButtons () {
-    const headerButtons = super._getHeaderButtons()
-    addPIDSheetHeaderButton(headerButtons, this)
-    return headerButtons
-  }
-
-  static get defaultOptions () {
-    return foundry.utils.mergeObject(super.defaultOptions, {
-      classes: ['Pendragon', 'sheet', 'item'],
+  static DEFAULT_OPTIONS = {
+    classes: ['Pendragon', 'sheet', 'item'],
+    position: {
       width: 520,
-      height: 570,
-      scrollY: ['.item-bottom-panel'],
-      tabs: [{navSelector: '.sheet-tabs',contentSelector: '.sheet-body',initial: 'attributes'}]
-    })
+      height: 570
+    },
+    tag: "form",
+    // automatically updates the item
+    form: {
+      submitOnChange: true,
+    },
+    window: {
+      resizable: true,
+    },
+    actions: {
+      onEditImage: this._onEditImage,
+      editPid: this._onEditPid,
+      deleteItem: PendragonClassSheet.#deleteItem
+    },
+    dragDrop: [{dropSelector: ".droppable"}]
+
   }
-  
-  /** @override */
-  get template () {
-    return `systems/Pendragon/templates/item/${this.item.type}.html`
+
+  static PARTS = {
+    header: {
+      template: "systems/Pendragon/templates/item/header.hbs"
+    },
+    tabs: {
+      template: 'templates/generic/tab-navigation.hbs',
+    },
+    // each tab gets its own template
+    attributes: {
+      template: 'systems/Pendragon/templates/item/class.attributes.hbs'
+    },
+    description: {
+      template: 'systems/Pendragon/templates/item/base.description.hbs'
+    },
+    gmTab: {
+      template: 'systems/Pendragon/templates/item/gmtab.hbs'
+    }
   }
-  
-  async getData () {
-    const sheetData = super.getData()
-    const itemData = sheetData.item
-    sheetData.hasOwner = this.item.isEmbedded === true
-    sheetData.isGM = game.user.isGM
-    sheetData.enrichedDescriptionValue = await TextEditor.enrichHTML(
-      sheetData.data.system.description,
-      {
-        async: true,
-        secrets: sheetData.editable
-      }
-    )
-    sheetData.enrichedGMDescriptionValue = await TextEditor.enrichHTML(
-      sheetData.data.system.GMdescription,
-      {
-        async: true,
-        secrets: sheetData.editable
-      }
-    )
+
+  async _prepareContext (options) {
+    // Default tab for first time it's rendered this session
+    if (!this.tabGroups.primary) this.tabGroups.primary = 'attributes';
+
+    let sheetData = {
+      ...await super._prepareContext(options),
+    }
+    const itemData = sheetData.item;
     const primary = [];
     const secondary = [];
     const tertiary = [];
@@ -88,75 +99,106 @@ export class PendragonClassSheet extends ItemSheet {
     sheetData.secondary = secondary
     sheetData.tertiary = tertiary
     sheetData.gears = gears
-    sheetData.gearsisEmpty = sheetData.gears.length ===0
+    sheetData.gearsisEmpty = sheetData.gears.length ===0;
+
+    // these two values could be set during _preparePartContext
+    sheetData.enrichedDescriptionValue = await TextEditor.enrichHTML(
+      this.item.system.description,
+      {
+        async: true,
+        secrets: sheetData.editable,
+        relativeTo: this.item
+      }
+    )
+    sheetData.enrichedGMDescriptionValue = await TextEditor.enrichHTML(
+      this.item.system.GMdescription,
+      {
+        async: true,
+        secrets: sheetData.editable
+      }
+    )
+    sheetData.tabs = this._initTabs('primary', ['attributes', 'description', 'gmTab']);
     return sheetData
   }
-  
-  /* -------------------------------------------- */
+
+  // this does the minimum currently, just sets the tab
+  // could also prepare tab-specific fields
+  async _preparePartContext(partId, context) {
+    switch (partId) {
+      case 'attributes':
+      case 'description':
+      case 'gmTab':
+        context.tab = context.tabs[partId];
+        break;
+      default:
+    }
+    return context;
+  }
+
   /**
   * Activate event listeners using the prepared sheet HTML
   * @param html {HTML}   The prepared HTML object ready to be rendered into the DOM
   */
-  activateListeners (html) {
-    super.activateListeners(html)
-    // Everything below here is only needed if the sheet is editable
-    if (!this.options.editable) return
-    html.find('.item-delete').click(event => this._onItemDelete(event))
-    const dragDrop = new DragDrop({
-      dropSelector: '.droppable',
-      callbacks: { drop: this._onDrop.bind(this) }
-    })
-    dragDrop.bind(html[0])
+  _onRender (context, _options) {
+    this.#dragDrop.forEach((d) => d.bind(this.element));
   }
-  
-  /* -------------------------------------------- */
-  
+  #createDragDropHandlers() {
+    return this.options.dragDrop.map((d) => {
+      d.permissions = {
+        //dragstart: this._canDragStart.bind(this),
+        drop: this._canDragDrop.bind(this),
+      };
+      d.callbacks = {
+        //dragstart: this._onDragStart.bind(this),
+        //dragover: this._onDragOver.bind(this),
+        drop: this._onDrop.bind(this),
+      };
+      return new DragDrop(d);
+    });
+  }
+  _canDragDrop(selector) {
+    return this.isEditable;
+  }
+
   //Allow for an item being dragged and dropped on to the sheet
-  async _onDrop (event, type = 'passion', collectionName = 'passions') {
+  async _onDrop (event) {
     event.preventDefault()
     event.stopPropagation()
-
-    let subType = event.currentTarget.dataset.collection
-    if (subType === "gear"){
+    let type = ['passion'];
+    const collectionName = event.currentTarget.dataset.collection ?? 'passions';
+    if(collectionName == 'gear') {
       type=["weapon","armour","horse","gear"]
-      collectionName = "gear"
     }
     const dataList = await PENUtilities.getDataFromDropEvent(event, 'Item')
     const collection = this.item.system[collectionName] ? foundry.utils.duplicate(this.item.system[collectionName]) : []
- 
+
     for (const item of dataList) {
-      if (!item || !item.system) continue
-      if (!(type).includes(item.type)) {continue}
+      if (!item || !item.system) continue;
+      if (!type.includes(item.type)) {continue}
 
       //If no PID then give warning and move to next item
       if (typeof(item.flags?.Pendragon?.pidFlag?.id)=== "undefined") {
         ui.notifications.warn(game.i18n.format('PEN.PIDFlag.noPID', {type:item.name}));
-        continue
+        continue;
       }
 
       //If Duplicate item then give warning and move to next item
       if (collection.find(el => el.pid === item.flags?.Pendragon?.pidFlag?.id)) {
         ui.notifications.warn(item.name + " : " +   game.i18n.localize('PEN.dupItem'));
-        continue
+        continue;
       }
 
       //Add item to collection
-      collection.push({name: item.name, oppName: item.system.oppName, uuid: item.uuid, pid:item.flags.Pendragon.pidFlag.id, subType: subType})
+      collection.push({name: item.name, oppName: item.system.oppName, uuid: item.uuid, pid:item.flags.Pendragon.pidFlag.id, subType: collectionName})
     }
     await this.item.update({ [`system.${collectionName}`]: collection })
   }
 
-  //Delete's a skill in the main skill list      
-  async _onItemDelete (event, collectionName = 'passions') {
-    const target = $(event.currentTarget).closest('.item')
-    if (target.data("collection")==='gear') {collectionName = 'gear'}
-    const itemId = target.data('item-id')
-    const itemIndex = this.item.system[collectionName].findIndex(itm => (itemId && itm.uuid === itemId))
-    if (itemIndex > -1) {
-      const collection = this.item.system[collectionName] ? foundry.utils.duplicate(this.item.system[collectionName]) : []
-      collection.splice(itemIndex, 1)
-      await this.item.update({ [`system.${collectionName}`]: collection })
-    }
+  //Delete an trait from the collection
+  static async #deleteItem(event, target) {
+    const {itemId} = target.closest("[data-item-id]")?.dataset ?? {};
+    const {collection} = target.closest('[data-collection]').dataset ?? {};
+    const coll = this.item.system[collection] ?? [];
+    await this.item.update({ [`system.${collection}`]: coll.filter(itm => itm.uuid != itemId) });
   }
-
 }
